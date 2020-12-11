20602a91dc636697c7caefe841b0ce1bc7a96846
yliuEmma <liu763@wisc.edu>
2020-12-11 12:32:09 -0500

    instance.py update?

--- instance.py
+++ instance.py
@@ -1,9 +1,10 @@
 import pixellib
 from pixellib.instance import instance_segmentation
-import cv2
+import time
 
-instance_seg = instance_segmentation()
-instance_seg.load_model("mask_rcnn_coco.h5")
-segmask, output = instance_seg.segmentImage("UWTEST.jpg", show_bboxes= True)
-cv2.imwrite("img.jpg", output)
-print(output.shape)
+segment_image = instance_segmentation()
+segment_image.load_model("mask_rcnn_coco.h5")
+start = time.perf_counter()
+segment_image.segmentImage("images/UWTEST.jpg", output_image_name = "maskRCNNUWTEST.jpg")
+time_took = time.perf_counter() - start
+print(f"Time took: {time_took:.2f}s")
